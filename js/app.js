bc8137026fd756a35ee7edb7ceec003eaab8f882
sadigzadeamina07 <Sadigzadeamina7@gmail.com>
2026-02-18 15:37:48 +0400

    changed position of cards

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@ let trendDIV=document.getElementById('trendDIV')
 
 trend.map((item)=>{
     trendDIV.innerHTML+=`
-    <div class="card  w-full md:w-[25%]">
-    <img src="${item.image}"  class="h-[50vh] object-cover bg-center" />
+    <div class="card  w-full md:w-[30%]">
+    <img src="${item.image}"  class="h-[90vh] md:h-[50vh] md:w-fit object-cover bg-top" />
     <p></p>
     <h3 class="font-bold  text-2xl">${item.title} </h3>
     </div>
